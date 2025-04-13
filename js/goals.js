1c55d65f029fbb53504fd40453e8f4e4e8d40d5f
Eric Sotoca <151804279+ericsotoca@users.noreply.github.com>
2025-04-13 18:19:44 +0200

    Update goals.js

--- js/goals.js
+++ js/goals.js
@@ -415,7 +415,11 @@ const GoalsApp = {
 
      escapeHTML: function(str) {
         if (typeof str !== 'string') return '';
-        return str.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>').replace(/"/g, '"').replace(/'/g, ''');
+        return str.replace(/&/g, '&amp;')
+                 .replace(/</g, '&lt;')
+                 .replace(/>/g, '&gt;')
+                 .replace(/"/g, '&quot;')
+                 .replace(/'/g, '&apos;');
     }
 
 }; // Fin de l'objet GoalsApp
